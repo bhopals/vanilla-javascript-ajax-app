74fc453b28ea6377915e45900aee9e2d75989443
Bhopal Singh <bhopalsinghsis@gmail.com>
2019-02-04 19:57:53 +0400

    Added XHR Success and Error Method

--- js/script2.js
+++ js/script2.js
@@ -19,11 +19,33 @@
 	//handle XHR Response
 	function responseMethod() {
 		if(httpRequest.readyState == 4) {
-			console.log(httpRequest.responseText);
+			if(httpRequest.status === 200) {
+				updatedUISuccess(httpRequest.responseText);
+			} else {
+				updateUIError();
+			}
+			console.log(httpRequest.responseText)
 		}
 	}
 
 
+	//handle XHR Success
+	function updatedUISuccess(responseText) {
+		var response = JSON.parse(responseText);
+		var condition = response.weather[0].main;
+		var degC = response.main.temp - 273.15;
+		var degCInt = Math.floor(degC);
+		var degF = degC * 1.8 + 32;
+		var degFInt = Math.floor(degF);
+		var weatherBox = document.getElementById('weather');
+		weatherBox.innerHTML = "<p>"+ degCInt + "&#176; C / "+ degFInt + "&#176; F </p> <p>"+condition+"</p>";
+	}
+
+	//handle XHR Error
+	function updateUIError(){
+		var weatherBox = document.getElementById('weather');
+		weatherBox.className = "hidden";
+	}
 
 })();
 
