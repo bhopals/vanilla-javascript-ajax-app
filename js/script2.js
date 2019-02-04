42dd20825875b41b6745aa2a6fde7edd306998ea
Bhopal Singh <bhopalsinghsis@gmail.com>
2019-02-04 20:47:53 +0400

    Fetch API

--- js/script2.js
+++ js/script2.js
@@ -1,5 +1,43 @@
 "use strict";
 
+//FETCH API
+
+//XHR REQUEST
+
+(function(){
+
+	var url = "http://api.openweathermap.org/data/2.5/weather?q=London,England";
+	var apiKey = "ed77c9da65098468569cdc788d5e39d1";
+	
+	fetch(url+'&appid='+apiKey).then(function(response){
+		if(!response.ok) {
+			throw Error(response.statusText);
+		}
+		return response.json();
+	}).then(function(response){
+		updatedUISuccess(response);
+	}).catch(function(error){
+		updateUIError();
+	});
+
+	function updateUIError(){
+		var weatherBox = document.getElementById('weather');
+		weatherBox.className = "hidden";
+	}
+	
+	function updatedUISuccess(response) {
+		var condition = response.weather[0].main;
+		var degC = response.main.temp - 273.15;
+		var degCInt = Math.floor(degC);
+		var degF = degC * 1.8 + 32;
+		var degFInt = Math.floor(degF);
+		var weatherBox = document.getElementById('weather');
+		weatherBox.innerHTML = "<p>"+ degCInt + "&#176; C / "+ degFInt + "&#176; F </p> <p>"+condition+"</p>";
+	}
+
+})();
+
+/*
 (function() {
 	var url = "http://api.openweathermap.org/data/2.5/weather?q=London,England";
 	var apiKey = "ed77c9da65098468569cdc788d5e39d1";
@@ -48,3 +86,4 @@
 
 })();
 
+*/
